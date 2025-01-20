674d9fb08dddd55726e16df47ebd366ad0923a2e
ssstarss <s.s.star@mail.ru>
2025-01-20 15:24:03 +0700

    init: project init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { hot } from "react-hot-loader"
+import React from "react";
+
+const App = () => {
+    
+    return (
+        <div>
+            <h2>Starting Develop: </h2>
+    
+        </div>
+    );
+};
+
+export default hot(module)(App);
